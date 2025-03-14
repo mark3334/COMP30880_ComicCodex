01889758be639e7bd680d1bdc567ace77d9292a9
Mark Kirwan <markkirwan333@gmail.com>
2025-03-14 12:08:01 +0000

    Configuration file duplicate code removed from Main

--- src/Main.java
+++ src/Main.java
@@ -4,50 +4,13 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, String> configMap = createHashMapConfig();
-
+        ConfigurationFile configFile = new ConfigurationFile();
+        Map<String, String> configMap = configFile.getConfigMap();
         // Printing the HashMap
         for (Map.Entry<String, String> entry : configMap.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
 
-    public static Map<String, String> createHashMapConfig() {
-        String configPath = "Config.txt";
-        Map<String, String> map = new HashMap<>();
-        BufferedReader fileReader = null;
-        try{
-            File file = new File(configPath);
-            fileReader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = fileReader.readLine()) != null) {
-                String[] pair = line.split(":");
-                if (pair.length == 2) {
-                    String key = pair[0];
-                    String value = pair[1];
-                    if (!key.isEmpty() && !value.isEmpty())
-                        map.put(key, value);
-                }
-                else{
-                    System.out.println("Mistake in config line");
-                }
 
-
-        }
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            if (fileReader != null) {
-                try {
-                    fileReader.close();
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return map;
-    }
 }
