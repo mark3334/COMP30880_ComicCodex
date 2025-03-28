9ea3c8b199429199b6f389d5929db2b085e2c7f3
Guanqiao Han <han254319205@gmail.com>
2025-03-28 13:49:21 +0000

    Create XMLGenerator

---
+++ src/main/java/comicXMLGenerator.java
@@ -0,0 +1,33 @@
+public class comicXMLGenerator {
+
+    public static String generateSceneXML(VignetteSchema schema, int sceneId) {
+        String leftPose = schema.getLeftPose();
+        String leftText = VignetteSchema.getRandomElement(schema.getLeftText());
+        String rightPose = VignetteSchema.getRandomElement(schema.getRightPose());
+        String background = VignetteSchema.getRandomElement(schema.getBackgrounds());
+        String combinedText = VignetteSchema.getRandomElement(schema.getCombinedText());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("<scene id=\"%d\">\n", sceneId));
+        sb.append(String.format("    <left pose=\"%s\">%s</left>\n",
+                leftPose, leftText));
+        sb.append(String.format("    <right pose=\"%s\"></right>\n",
+                rightPose));
+        sb.append(String.format("    <background>%s</background>\n",
+                background));
+        sb.append(String.format("    <combinedText>%s</combinedText>\n",
+                combinedText));
+        sb.append("</scene>");
+        return sb.toString();
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        VignetteManager vignetteManager = new VignetteManager();
+        VignetteSchema schema = vignetteManager.getRandomSchema();
+        System.out.println(schema);
+
+        String xml = generateSceneXML(schema, 1);
+        System.out.println(xml);
+    }
+}
