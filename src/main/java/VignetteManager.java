0d8d036e720ece5b5e29cbd062cacc61ed5253df
AbdullahShinwari8970 <abdullah.shinwari@ucdconnect.ie>
2025-03-31 16:23:04 +0100

    Resolved Merge Conflict Issue

--- src/main/java/VignetteManager.java
+++ src/main/java/VignetteManager.java
@@ -5,36 +5,27 @@ public class VignetteManager {
     private String filePath;
     private List<VignetteSchema> vignetteSchemas; //A list that will hold VignetteScheme Objects from the file.
     private Map<String, String> translationMap;
-    private TranslationFile  t;
+
     public VignetteManager() {
         ConfigurationFile configFile = ConfigurationFile.getInstance();
-<<<<<<< HEAD
         this.filePath = configFile.getValueByKey("WORD_ASSET_MAPPING"); //Loading file path for vignette data.
-=======
-        File root = Helper.getRootDirectory();
-        String pathFromRoot = configFile.getValueByKey("WORD_ASSET_MAPPING");
-        this.filePath = new File(root, pathFromRoot).getAbsolutePath();
->>>>>>> 6bc858d9cf12b661950f7fc426cc5e913a88844e
         File file = new File(this.filePath);
         this.vignetteSchemas = new ArrayList<>();
         boolean append = false;
         FileParser.readFileToVignetteSchemas(file, this.vignetteSchemas, append);
 
-        this.t = TranslationFile.getInstance();
+        this.translationMap = new HashMap<>();
+        String folderPath = configFile.getValueByKey("TRANSLATIONS_PATH");
+        String translationFileName = "English_Spanish";
+        File translationFile = new File(folderPath, translationFileName);
+        FileParser.fileToHashmap(translationFile, this.translationMap, false);
+    }
 
-<<<<<<< HEAD
 
     /**
      *
      * @return a random element from the list, or null if empty
      */
-=======
-    }
-    public static String translate(String s){
-        TranslationFile t = TranslationFile.getInstance();
-        return t.translate(s);
-    }
->>>>>>> 6bc858d9cf12b661950f7fc426cc5e913a88844e
     public List<VignetteSchema> getVignetteSchemas(){
         return this.vignetteSchemas;
     }
@@ -58,7 +49,6 @@ public class VignetteManager {
         }
     }
 
-<<<<<<< HEAD
     /*
         Returns the spanish translation for english text.
      */
@@ -68,15 +58,11 @@ public class VignetteManager {
      */
     public String translateToSpanish(String english) {
         return translationMap.get(english);
-=======
-    public String translateToTarget(String inp) {
-        return this.t.translate(inp);
->>>>>>> 6bc858d9cf12b661950f7fc426cc5e913a88844e
     }
 
     public static void main(String[] args) {
         VignetteManager text_reader = new VignetteManager();
-        System.out.println(text_reader.t.translate("His expression looks determined."));
+        System.out.println(text_reader.translateToSpanish("His expression looks determined."));
         //text_reader.printAll();
     }
 }
