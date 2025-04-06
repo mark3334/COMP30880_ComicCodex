edd311584bd9ad2344a05e4447d1950cae8a53ea
AbdullahShinwari8970 <abdullah.shinwari@ucdconnect.ie>
2025-04-06 17:24:22 +0100

    testGenerateSceneXML Passed.

--- src/test/java/ComicXMLGeneratorTest.java
+++ src/test/java/ComicXMLGeneratorTest.java
@@ -29,4 +29,40 @@ public class ComicXMLGeneratorTest {
         Element importedScene = (Element) root.getFirstChild();
         Assertions.assertEquals("scene", importedScene.getNodeName(), "Child of <comic> should be <scene>");
     }
+
+    @Test
+    public void testGenerateSceneXML() throws ParserConfigurationException {
+
+        //No left text scene.
+        VignetteSchema testSchema = new VignetteSchema("Giga_Chad", "Wow", "", "Suprised","Gym");
+//        System.out.println(testSchema.toString());
+//        System.out.println(Arrays.toString(SceneGeneratorManager.getFeatureVector(testSchema)));
+
+
+        //Only combined text scene.
+        VignetteSchema testSchema2 = new VignetteSchema("Giga_Chad", "Wow", "", "","Gym");
+//        System.out.println(testSchema2.toString());
+//        System.out.println(Arrays.toString(SceneGeneratorManager.getFeatureVector(testSchema2)));
+
+
+        //Only left text scene.
+        VignetteSchema testSchema3 = new VignetteSchema("Giga_Chad", "", "Wow", "","");
+//        System.out.println(testSchema3.toString());
+//        System.out.println(Arrays.toString(SceneGeneratorManager.getFeatureVector(testSchema3)));
+
+        //Full text Scene.
+        VignetteSchema testSchema4 = new VignetteSchema("Giga_Chad", "Wow", "Wow", "Suprised","Gym");
+//        System.out.println(testSchema4.toString());
+//        System.out.println(Arrays.toString(SceneGeneratorManager.getFeatureVector(testSchema4)));
+
+
+        VignetteSchema[] schemas = {
+                testSchema, testSchema2, testSchema3, testSchema4
+        };
+
+        for (int i = 0; i < schemas.length; i++) {
+            Document result = ComicXMLGenerator.generateSceneXML(schemas[i]);
+            assertNotNull(result, "Result " + (i + 1) + " should not be null");
+        }
+    }
 }
