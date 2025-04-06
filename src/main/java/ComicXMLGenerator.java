7ce4cd3a9541e61bac1d5c3935d36285c348e961
AbdullahShinwari8970 <abdullah.shinwari@ucdconnect.ie>
2025-04-06 18:50:31 +0100

    Refactored writeXmlToFolder, in prep for tests.

--- src/main/java/ComicXMLGenerator.java
+++ src/main/java/ComicXMLGenerator.java
@@ -11,10 +11,8 @@ import javax.xml.transform.stream.StreamResult;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-public class comicXMLGenerator {
+public class ComicXMLGenerator {
 
     /**
      * Generates a DOM XML Document from a given VignetteSchema.
@@ -37,16 +35,15 @@ public class comicXMLGenerator {
      *
      * @throws TransformerException if there is an issue writing XML.
      * @throws IOException if there is a problem creating the output directory.
-     * @throws ParserConfigurationException if there is an error building XML.
      */
-    public static void writeXmlToFolder() throws TransformerException, IOException, ParserConfigurationException {
-    File root = Helper.getRootDirectory();
-    File folder = new File(root, "Resources/XMLoutput");
-
-    //Get a random vignette schema and generate its scene.
-    VignetteSchema schema = new VignetteManager().getRandomSchema();
-    Document sceneDoc = generateSceneXML(schema);
-    Document fullDoc = wrapSceneWithComic(sceneDoc);
+    public static void writeXmlToFolder(Document doc, String filename, File folder) throws TransformerException, IOException {
+//    File root = Helper.getRootDirectory();
+//    File folder = new File(root, "Resources/XMLoutput");
+//
+//    //Get a random vignette schema and generate its scene.
+//    VignetteSchema schema = new VignetteManager().getRandomSchema();
+//    Document sceneDoc = generateSceneXML(schema);
+//    Document fullDoc = wrapSceneWithComic(sceneDoc);
 
     //Create the output folder if it doesn't exist
     if (!folder.exists()) {
@@ -58,15 +55,17 @@ public class comicXMLGenerator {
         }
     }
 
+    File outputFile = new File(folder, filename);
+
     //Create a timestamped filename
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
-    String timestamp = LocalDateTime.now().format(formatter);
-    File outputFile = new File(folder, "scene_" + timestamp + ".xml");
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+//    String timestamp = LocalDateTime.now().format(formatter);
+//    File outputFile = new File(folder, "scene_" + timestamp + ".xml");
 
     Transformer transformer = TransformerFactory.newInstance().newTransformer();
     transformer.setOutputProperty(OutputKeys.INDENT, "yes");
     transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-    transformer.transform(new DOMSource(fullDoc), new StreamResult(outputFile));
+    transformer.transform(new DOMSource(doc), new StreamResult(outputFile));
 
     System.out.println("XML written to: " + outputFile.getAbsolutePath());
 }
@@ -79,7 +78,7 @@ public class comicXMLGenerator {
      * @return A new Document with <comic> as the root and <scene> nested inside.
      * @throws ParserConfigurationException if document creation fails.
      */
-    private static Document wrapSceneWithComic(Document sceneDoc) throws ParserConfigurationException {
+    public static Document wrapSceneWithComic(Document sceneDoc) throws ParserConfigurationException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document fullDoc = builder.newDocument();
